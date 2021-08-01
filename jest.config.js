771afbe4e4c82b959c8939847a9728a61a668b0b
Michael Duve <mduve@designmail.net>
2021-08-01 14:20:22 +0200

    fix(dependencies): bumped dependencies
    BREAKING CHANGE:
    
    - Node 10 is required
    
    Chore:
    - replace uuid by nanoid
    - upgrade fast-glob, gzip-size, lodash, chalk, and arg to latest versions
    - upgraded dev dependencies to latest versions
    
    Other changes:
    
    - only do a semantic release on branch release
    - deploy gh-pages only on branch release
    - codebase now runs on 12.22.1

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageReporters: ['json', 'lcov'],
+  coverageReporters: ['json', 'lcov', 'text'],
   verbose: true,
+  rootDir: './src',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/cli.js', 'src/index.js'],
+  collectCoverageFrom: ['*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
 };
